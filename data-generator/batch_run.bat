a30dd2105020db54ef27f023d745536bd84d273a
Ryan Pereira <rayoz12@yahoo.com.au>
2021-06-08 17:48:56 +1000

    Data generator script

---
+++ data-generator/batch_run.bat
@@ -0,0 +1,3 @@
+node .\index.js ..\exportedCropData\cropping_small\cropping_small.geojson
+node .\index.js ..\exportedCropData\cropping_med\cropping_med.geojson
+node .\index.js ..\exportedCropData\cropping_large\cropping_large.geojson
