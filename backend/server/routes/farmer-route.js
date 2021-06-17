c15964ce2bb3b374cd00d67c12f0401bb79bc245
Ryan Pereira <rayoz12@yahoo.com.au>
2021-06-17 11:15:29 +1000

    Add Farmer Route

---
+++ backend/server/routes/farmer-route.js
@@ -0,0 +1,34 @@
+// initialize Cloudant
+const { CloudantV1 } = require("@ibm-cloud/cloudant");
+const client = CloudantV1.newInstance({});
+
+const APPLICATION_DB = "application-db";
+const db = APPLICATION_DB;
+
+var express = require('express');
+var router = express.Router();
+
+router.get("/", async (req, res) => {
+    const farmers = await client.postPartitionAllDocs({
+        db,
+        includeDocs: true,
+        partitionKey: "farmer"
+    });
+    res.send(farmers.result);
+});
+
+router.get("/:id", async (req, res) => {
+    const id = req.params["id"];
+    if (!id) {
+        res.sendStatus(400).end();
+        return;
+    }
+    const farmer = await client.getDocument({
+        db,
+        docId: `farmer:${id}`
+    });
+    res.send(farmer.result);
+});
+
+
+module.exports = router;
