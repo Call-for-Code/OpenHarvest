fa9d18b6db1dc6d7d39b6ba0ff59ee1b5345db2b
Tyler Philips <tylerphilips@Tylers-MacBook-Pro.local>
2021-06-19 01:40:59 -0400

    bar graph

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -43,8 +43,45 @@ export class DashboardComponent implements OnInit {
 			"label": "Browsers"
 		  }
 		},
-		"height": "400px"
+		"height": "100px"
 	  };
+	
+	data2 = [
+		{
+				"group": "Qty",
+				"value": 65000
+		},
+		{
+				"group": "More",
+				"value": 29123
+		},
+		{
+				"group": "Sold",
+				"value": 35213
+		},
+		{
+				"group": "Restocking",
+				"value": 51213
+		},
+		{
+				"group": "Misc",
+				"value": 16932
+		}
+	];
+		options2 = {
+			"title": "Vertical simple bar (discrete)",
+			"axes": {
+					"left": {
+							"mapsTo": "value"
+					},
+					"bottom": {
+							"mapsTo": "group",
+							"scaleType": "labels"
+					}
+			},
+			"height": "400px"
+		};
+	
 
   constructor(protected modalService: ModalService) { }
 
