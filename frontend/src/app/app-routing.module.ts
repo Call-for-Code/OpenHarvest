d3a47dc770252da2a859a11831be6835d07b8a64
Ryan Pereira <rayoz12@yahoo.com.au>
2021-06-20 09:39:34 +1000

    Rebase of latest

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,8 @@ import { CatalogComponent } from './pages/catalog/catalog.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { SupportComponent } from './pages/support/support.component';
 import { Link1Component } from './pages/link1/link1.component';
-<<<<<<< HEAD:frontend/src/app/app-routing.module.ts
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
 import { LandAreasComponent } from "./pages/land-areas/land-areas.component";
->>>>>>> progress:carbon-frontend/src/app/app-routing.module.ts
 
 const routes: Routes = [
 	{
