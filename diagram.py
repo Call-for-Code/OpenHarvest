55b2710095401f71417ac1aee1e724230dfbf159
Ryan Pereira <ryan.pereira@ibm.com>
2021-06-23 16:21:14 +1000

    Fix Diagram

--- diagram.py
+++ diagram.py
@@ -1,11 +1,6 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Diagram, Cluster
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+from diagrams.ibm.general import Cloudant
+
+with Diagram("Open Harvest", show=True, direction="TB"):
+    with Cluster("Backend"):
