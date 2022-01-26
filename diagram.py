3217a95d2f975a9e52a3457f4a82b57ccd793907
Ryan Pereira <ryan.pereira@ibm.com>
2022-01-27 10:56:34 +1100

    Forced commit for Angular 13

--- diagram.py
+++ diagram.py
@@ -1,6 +1,19 @@
 from diagrams import Diagram, Cluster
 
 from diagrams.ibm.general import Cloudant
+from diagrams.programming.language import Javascript
+from diagrams.generic.compute import Rack
 
-with Diagram("Open Harvest", show=True, direction="TB"):
-    with Cluster("Backend"):
+with Diagram("Open Harvest - Runtime", show=True, direction="TB"):
+
+    with Cluster("IBM Cloud Services"):
+        cloudant_app_db = Cloudant("Application-DB")
+        cloudant_lot_areas = Cloudant("Lot-Areas")
+
+    with Cluster("Node.js Backend"):
+        cloudantSDK = Javascript("CloudantSDK")
+        express
+
+
+
+
