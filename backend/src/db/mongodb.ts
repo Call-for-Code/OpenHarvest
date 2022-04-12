2cf1a75eccdf2591f9ef95eb1bce637ba24b5fba
Ryan Pereira <ryan.pereira@ibm.com>
2022-04-13 09:58:52 +1000

    Update mongodb ssl to use a path.

--- backend/src/db/mongodb.ts
+++ backend/src/db/mongodb.ts
@@ -6,15 +6,15 @@ export async function mongoInit() {
     // console.log(process.env.mongodb_url);
     if (process.env.NODE_ENV == "production") {
         // We have to write the SSL cert contents so that mongoose can read it again...
-        if (process.env.mongodb_sslCA == undefined) {
-            throw new Error("Please define mongodb_sslCA in the environment with the SSL !")
+        if (process.env.mongodb_sslCA_Path == undefined) {
+            throw new Error("Please define mongodb_sslCA_Path in the environment with the SSL !")
         }
-        const CACertStr = process.env.mongodb_sslCA.replace("\\n", "\n"); // replace text with actual new lines.
-        await writeFile("mongoose_sslCA", CACertStr);
+        // const CACertStr = process.env.mongodb_sslCA.replace("\\n", "\n"); // replace text with actual new lines.
+        // await writeFile("mongoose_sslCA", CACertStr);
 
         await connect(process.env.mongodb_url!!, {
             sslValidate: false,
-            sslCA: "mongoose_sslCA"
+            sslCA: process.env.mongodb_sslCA_Path
         });
     }
     else {
