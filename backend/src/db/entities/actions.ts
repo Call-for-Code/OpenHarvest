e7943e9632074042427341d8cf5c4b4e164c49e4
JonathanScialpi <scialpi27@gmail.com>
2022-05-25 15:45:12 -0400

    initil data objects to store action weights, actions taken, and payloads generated
    Signed-off-by: JonathanScialpi <scialpi27@gmail.com>

---
+++ backend/src/db/entities/actions.ts
@@ -0,0 +1,28 @@
+
+import { Schema, model, ObjectId, Types } from 'mongoose';
+
+const ObjectId = Schema.Types.ObjectId;
+
+export interface Actions {
+    _id?: Types.ObjectId;
+    farmer_id: string;
+    action_a_completed: boolean;
+    action_b_completed: boolean;
+    action_c_completed: boolean;
+    action_d_completed: boolean;
+}
+
+export const ActionsSchema = new Schema({
+    _id: {
+        type: ObjectId,
+        auto: true
+    },
+    farmer_id: String,
+    action_a_completed: Boolean,
+    action_b_completed: Boolean,
+    action_c_completed: Boolean,
+    action_d_completed: Boolean,
+
+});
+
+export const ActionsModel = model<Actions>("actions", ActionsSchema);
