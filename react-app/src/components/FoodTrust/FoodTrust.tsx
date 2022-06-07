c10aa2f12febf6daf7137a379e3a4e2b68f80b73
JonathanScialpi <scialpi27@gmail.com>
2022-06-07 14:16:18 -0400

    moving crop template to crop components and removing from foodtrust page
    Signed-off-by: JonathanScialpi <scialpi27@gmail.com>

--- react-app/src/components/FoodTrust/FoodTrust.tsx
+++ react-app/src/components/FoodTrust/FoodTrust.tsx
@@ -4,7 +4,6 @@ import { PageTitleBar, Grid, Column, Row, RadioButton, RadioButtonGroup, Select,
 import {Information16} from '@carbon/icons-react';
 import {BusinessEvents, UnitsOfMeasure, EventTypeModalText, PackingIDModalText, AssetIdModalText} from './FoodTrustHelperData';
 import { FoodTrustAPI } from "../../services/food-trust";
-import FarmerActions from "../Farmers/CropTemplate"
 
 const foodTrustAPI = new FoodTrustAPI()
 
@@ -267,7 +266,6 @@ const FoodTrust = () => {
                     </Column>
                 </Row>
             </Grid>
-            <FarmerActions/>
         </>
     );
 }
