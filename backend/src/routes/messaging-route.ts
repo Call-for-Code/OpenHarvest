0c1a1b5e8f55b5de187dc0a91aadae427131d0c1
rayoz12 <rayoz12@yahoo.com.au>
2022-07-18 18:40:07 +1000

    Add thread index util functions Implement messaging using a thread model which groups DMs and group chats under a single thread. Fix issues in NewConvo Group creation.
    Signed-off-by: rayoz12 <rayoz12@yahoo.com.au>

--- backend/src/routes/messaging-route.ts
+++ backend/src/routes/messaging-route.ts
@@ -1,9 +1,9 @@
 // import dependencies and initialize the express router
 import { Router } from "express";
 import { TwilioInstance } from "./../integrations/twilio/twilio.service";
-import { MessageLogModel } from "../db/entities/messageLog";
-import { MessageGroup, MessageGroupModel } from "../db/entities/messageGroup";
-import { FarmerModel } from "../db/entities/farmer";
+import { MessageLog, MessageLogModel } from "../db/entities/messageLog";
+import { getFarmerIdsFromIndex, getIndexFromFarmerIds, MessageGroup, MessageGroupModel } from "../db/entities/messageGroup";
+import { FarmerModel, Farmer } from "../db/entities/farmer";
 
 // We need to create and run a particular instance based on configuration.
 // Or intelligently by looking at the number we're sending to.
@@ -34,12 +34,99 @@ router.post("/sendSMSToFarmer", async (req, res) => {
     }
 });
 
+export interface ThreadsDTO {
+    /**
+     * This is either the farmer_id or the group_id (depending on if this isn't a group thread or not)
+     */
+    thread_id: string;
+    farmers: Farmer[];
+    isGroup: boolean;
+    preview: string;
+    messages: MessageLog[];
+}
+
 /** 
  * This endpoint organises the messages into their groups and sends it
  * If a message doesn't have a group it's put into a farmer
  */
 router.get("/threads", async (req, res) => {
+    const messages = await MessageLogModel.find({}).lean().exec();
+    const groups = await MessageGroupModel.find({}).lean().exec();
+    // Group_id to index
+    const groupLookup: {[key: string]: string} = {};
+
+    const threadMap: {[index: string]: ThreadsDTO} = {};
+
+    for (const group of groups) {
+        const index = getIndexFromFarmerIds(group.farmer_ids);
+        if (!(index in groupLookup)) {
+            groupLookup[group._id.toString()] = index;
+        } 
+    }  
+
+    // Iterate through the messages
+    for (const message of messages) {
+        // Get the index first
+        let index = "INVALID";
+        if (message.group_id === null && message.farmer_id !== null) {
+            // This is a DM
+            // The farmer is the index
+            index = message.farmer_id;
+            if (!(index in threadMap)) {
+                const thread: ThreadsDTO = {
+                    thread_id: index,
+                    farmers: [],
+                    isGroup: false,
+                    preview: "",
+                    messages: []
+                }
+                threadMap[index] = thread;
+            }
+        }
+        else if (message.group_id !== null && message.farmer_id === null) {
+            // This is a group message
+            // The combined
+            index = groupLookup[message.group_id];
+            if (!(index in threadMap)) {
+                const thread: ThreadsDTO = {
+                    thread_id: index,
+                    farmers: [],
+                    isGroup: false,
+                    preview: "",
+                    messages: []
+                }
+                threadMap[index] = thread;
+            }
+        }
+        else {
+            console.error(`[Message Route] Unable to determine if message is a DM or group message: ${message._id}`);
+            continue;
+        }
+
+        threadMap[index].messages.push(message);
+    }
+
+    // Get the farmers and add it to the thread
+    const threads: ThreadsDTO[] = Array.from(Object.values(threadMap));
+    const farmer_ids = threads.map(it => it.isGroup ? getFarmerIdsFromIndex(it.thread_id) : it.thread_id).flat();    
+    const farmers = await FarmerModel.find({_id: {$in: farmer_ids}});
+    const farmerLookup: {[key: string]: Farmer} = {};
+    for (const farmer of farmers) {
+        farmerLookup[farmer._id.toString()] = farmer;
+    }
+
+    for (let i = 0; i < threads.length; i++) {
+        const thread = threads[i];
+        if (thread.isGroup) {
+            const ids = getFarmerIdsFromIndex(thread.thread_id);
+            thread.farmers = ids.map(it => farmerLookup[it]);
+        }
+        else {
+            thread.farmers = [farmerLookup[thread.thread_id]];
+        }
+    }
     
+    res.json(threads);
 });
 
 export interface CreateConversationThreadDTO {
@@ -50,7 +137,7 @@ export interface CreateConversationThreadDTO {
 /**
  * This creates a thread with an inital message. Threads with no messages are orphans and shouldn't exist
  */
-router.post('/threads', async (req, res) => {
+router.post('/new-thread', async (req, res) => {
     const body: CreateConversationThreadDTO = req.body;
 
     // Ensure farmer_ids exist
@@ -60,16 +147,86 @@ router.post('/threads', async (req, res) => {
         return res.status(400).end("Farmer not Found!");
     }
 
-    // Lets create the group
-    const group = await MessageGroupModel.create({
-        farmer_ids: body.farmer_ids
-    });
+    // Find an existing group or create one
+    const index = getIndexFromFarmerIds(body.farmer_ids);
+    let group = await MessageGroupModel.findOne({farmer_id_index: index});
+    if (group === null) {
+        // Lets create the group
+        group = await MessageGroupModel.create({
+            farmer_ids: body.farmer_ids
+        });
+    }
 
     group.farmers = farmers;
 
-    const messageLogs = await TwilioInstance.sendMessageToGroup(group, body.message);
+    try {
+        const messageLogs = await TwilioInstance.sendMessageToGroup(group, body.message);
+        
+        const thread: ThreadsDTO = {
+            thread_id: index,
+            farmers,
+            isGroup: false,
+            preview: body.message,
+            messages: messageLogs
+        }
 
-    return res.json(messageLogs.map(it => it.messageRef));
+        // return res.json(messageLogs.map(it => it.messageRef));
+        return res.json(thread);
+    }
+    catch (e: any) {
+        res.status(500).send(e).end();
+    }
+
+});
+
+export interface SendMessageToThreadDTO {
+    thread_id: string;
+    message: string;
+};
+
+router.post('/thread', async (req, res) => {
+    const body: SendMessageToThreadDTO = req.body;
+    if (!body.thread_id) {
+        return res.status(400).end("Missing thread_id");
+    }
+    if (!body.message) {
+        return res.status(400).end("Missing message");
+    }
+    const {thread_id, message} = body;
+
+    const isGroup = thread_id.includes("|");
+
+    if (isGroup) {
+        const group = await MessageGroupModel.findOne({farmer_id_index: thread_id});
+        if (group === null) {
+            return res.status(400).end("Unknown thread_id");
+        }
+        const farmers = await FarmerModel.find({_id: {$in: group.farmer_ids}});
+        group.farmers = farmers;
+
+        try {
+            const messageLogs = await TwilioInstance.sendMessageToGroup(group, body.message);
+            return res.json(messageLogs.map(it => it.messageRef));
+        }
+        catch (e: any) {
+            res.status(500).send(e).end();
+        }
+    }
+    else {
+        const farmer = await FarmerModel.findById(thread_id);
+        if (farmer === null) {
+            return res.status(400).end("Unknown thread_id");
+        }
+
+        try {
+            const messageLog = await TwilioInstance.sendMessageToFarmer(farmer, message);
+            res.json(messageLog)
+        }
+        catch (e: any) {
+            res.status(500).send(e).end();
+        }
+    }
+
 
 });
 
--- react-app/src/services/messageLog.ts
+++ react-app/src/services/messageLog.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Farmer } from './farmers';
 
 export enum Source {
     Farmer = "Farmer",
@@ -19,6 +20,17 @@ export interface MessageLog {
     timestamp: Date;
 }
 
+export interface ThreadsDTO {
+    /**
+     * This is either the farmer_id or the group_id (depending on if this isn't a group thread or not)
+     */
+    thread_id: string;
+    farmers: Farmer[];
+    isGroup: boolean;
+    preview: string;
+    messages: MessageLog[];
+}
+
 export async function getAllMessages(): Promise<MessageLog[]> {
     const res = await fetch("/api/messaging/");
     const data: MessageLog[] = await res.json();
@@ -29,6 +41,12 @@ export async function getAllMessages(): Promise<MessageLog[]> {
     return data;
 }
 
+export async function getThreads(): Promise<ThreadsDTO[]> {
+    const req = await axios.get("/api/messaging/threads");
+    const data = req.data;
+    return data;
+}
+
 export async function sendMessageToFarmer(farmer_id: string, message: string): Promise<MessageLog> {
     if (farmer_id == undefined || message == undefined) {
         throw new Error("Params are undefined!");
@@ -36,6 +54,24 @@ export async function sendMessageToFarmer(farmer_id: string, message: string): P
 
     const req = await axios.post("/api/messaging/sendSMSToFarmer", {
         farmer_id,
+        message
+    });
+    const data = req.data;
+    return data;
+}
+
+export async function sendMessageToNewGroup(farmer_ids: string[], message: string) {
+    const req = await axios.post("/api/messaging/new-thread", {
+        farmer_ids,
+        message
+    });
+    const data = req.data;
+    return data;
+}
+
+export async function sendMessageToGroup(thread_id: string, message: string) {
+    const req = await axios.post("/api/messaging/thread", {
+        thread_id,
         message
     });
     const data = req.data;
