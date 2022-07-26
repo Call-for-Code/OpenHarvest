c6256c69693972f963c4b9a26820a9e5b5bf1ee8
rayoz12 <rayoz12@yahoo.com.au>
2022-07-26 21:02:48 +1000

    Messaging fixes
    Signed-off-by: rayoz12 <rayoz12@yahoo.com.au>

--- react-app/src/components/Messaging/Conversation.tsx
+++ react-app/src/components/Messaging/Conversation.tsx
@@ -14,17 +14,19 @@ export function Conversation(props: ConversationProps) {
         messages = [];
         for (let i = 0; i < props.conversation.messages.length; i++) {
             const message = props.conversation.messages[i];
-            const existing = messages.find(it => it.group_id === message.group_id && it.message === message.message);
+            const existing = messages.find(it => it.group_id !== null && it.group_id === message.group_id && it.message === message.message);
             if (existing) {
                 continue;
             }
             else {
                 messages.push(message);
             }
+            
+            // messages.push(message);
         }
     }
 
-    return <div className="flex flex-col justify-end h-full">
+    return <div className="flex flex-col justify-end h-full overflow-auto">
         {messages && messages.map(it => {
             if (it.source == Source.OpenHarvest) {
                 return <div key={it._id} className="bg-[#3D3D3D] rounded-[7px] w-auto max-w-[70%] h-auto text-[13px] rounded-br-none px-[10px] py-[10px] self-end mb-[20px] text-white shadow">{it.message}</div>
